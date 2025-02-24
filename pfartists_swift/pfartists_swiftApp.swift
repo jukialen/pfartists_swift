2a8d69b80d824e3c1288e9b023fa32d109004c1c
jukialen <pbidny@gmail.com>
2025-02-24 11:15:19 +0100

    Initial Commit

---
+++ pfartists_swift/pfartists_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  pfartists_swiftApp.swift
+//  pfartists_swift
+//
+//  Created by Piotr prom on 2025/02/24.
+//
+
+import SwiftUI
+
+@main
+struct pfartists_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pfartists_swiftTests/pfartists_swiftTests.swift
@@ -0,0 +1,16 @@
+//
+//  pfartists_swiftTests.swift
+//  pfartists_swiftTests
+//
+//  Created by Piotr prom on 2025/02/24.
+//
+
+import Testing
+
+struct pfartists_swiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
